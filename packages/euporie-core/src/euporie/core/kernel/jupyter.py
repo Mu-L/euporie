debf18197694d2282d69ac376d804846acc13ede
Josiah Outram Halstead <josiah@halstead.email>
2026-05-19 10:21:28 +0100

    Fix MacOS readline cooked mode workaround

--- packages/euporie-core/src/euporie/core/kernel/jupyter.py
+++ packages/euporie-core/src/euporie/core/kernel/jupyter.py
@@ -140,14 +140,13 @@ class JupyterKernel(BaseKernel):
         self._debug_seq = 0
 
         # Accessing the kernel spec causes `readline` to be imported, which causes the
-        # terminal to be set to cooked mode on MacOS when run not on the main thread.
+        # terminal to be set to cooked mode on MacOS.
         # The import  process leading to this is:
         #   jupyter_client -> ipykernel -> ipython.core.debugger -> pdb -> readline
         # We deliberately access the kernelspec here in cooked mode, causing ptk to
         # return the terminal to raw mode when done
-        if threading.current_thread() != threading.main_thread():
-            with self.kernel_tab.app.input.cooked_mode():
-                _ = self.km.kernel_spec
+        with self.kernel_tab.app.input.cooked_mode():
+            _ = self.km.kernel_spec
 
     def _set_living_status(self, alive: bool) -> None:
         """Set the life status of the kernel."""
